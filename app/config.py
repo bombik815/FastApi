2ef7fd4e832919342c1a27fbd06183795fc2f1db
bombik815 <bombik815@gmail.com>
2024-05-27 16:25:08 +0300

    change models for table users, bookings, hotels, rooms

--- app/config.py
+++ app/config.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# load_dotenv()
 
 class Settings(BaseSettings):
     DB_HOST: str
--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,13 +1,19 @@
-from sqlalchemy import Column, Integer, String, JSON
-from app.database import Base
+from sqlalchemy import JSON, Column, Integer, String
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 
+from app.database import Base
 
 class Hotels(Base):
     __tablename__ = "hotels"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    location = Column(String, nullable=False)
-    services = Column(JSON)
-    room_quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str]
+    location: Mapped[str]
+    services: Mapped[list[str]] = mapped_column(JSON)
+    rooms_quantity: Mapped[int]
+    image_id: Mapped[int]
+
+    rooms: Mapped[list["Rooms"]] = relationship(back_populates="hotel")
+
+    def __str__(self):
+        return f"Отель {self.name} {self.location[:30]}"
