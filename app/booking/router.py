e9d88791fab893bfbf065b5281d52faf39a976b4
bombik815 <bombik815@gmail.com>
2024-06-26 14:27:21 +0300

    add function add booking in databse

--- app/booking/router.py
+++ app/booking/router.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends
 
 from app.booking.DAO import BookingDAO
 from app.booking.schemas import SBooking
+from app.exceptions import RoomCannotBeBooked
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -12,5 +15,22 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookins(user: Users = Depends(get_current_user)):  # -> list[SBooking]:
+async def get_bookins(user: Users = Depends(get_current_user)) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(
+        room_id: int, date_from: date, date_to: date,
+        user: Users = Depends(get_current_user)):
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+
+    if not booking:
+        raise RoomCannotBeBooked
+    # TypeAdapter и model_dump - это новинки новой версии Pydantic 2.0
+    # booking = TypeAdapter(SNewBooking).validate_python(booking).model_dump()
+    # Celery - отдельная библиотека
+    # send_booking_confirmation_email.delay(booking, user.email)
+    # Background Tasks - встроено в FastAPI
+    # background_tasks.add_task(send_booking_confirmation_email, booking, user.email)
+    # return booking
